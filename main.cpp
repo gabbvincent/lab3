7ea61d42e163063bdb156c17eecbc2e551c32bb5
Vincent Gabb <60416073+gabbvincent@users.noreply.github.com>
2020-09-13 00:26:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <string>
 using std::string;
+using namespace std;
 
 // COMPLETE EACH OF THE FOLLOWING FUNCTIONS
 
@@ -14,29 +15,67 @@ using std::string;
 // If name param is empty string, return "Nice to meet you."
 string greet(string name) {
 
+if (name != "") {
+
+  cout << "Nice to meet you, " << name << ".";
+
+} else {
+
+  return "Nice to meet you.";
+
 }
+return 0;
+}
+
 
 // Return true if denominator is a factor of numerator.
 // Return false otherwise.
 bool isFactor(int numerator, int denominator) {
 
+  if ((numerator / denominator) % 2 == 0) {
+  
+  return true;
+
+  } else {
+  
+ return false;
+} 
+
 }
 
-// Given an initial value, compute how many quarters, dimes, nickels, and
-// pennies would be given as change.
+// Given an initial value, .compute how many quarters, dimes, nickels, and
+// pennies would be given as change
 void makeChange(unsigned int initialValue, unsigned int &quarters,
                 unsigned int &dimes, unsigned int &nickels,
                 unsigned int &pennies) {
 
+      int left_Over;
+
+      quarters = initialValue / 25;
+      left_Over = initialValue % 25;
+
+      dimes = left_Over / 10;
+      left_Over = left_Over % 10;
+
+      nickels = left_Over / 5;
+      left_Over = left_Over % 5;
+
+      pennies = left_Over;
+
+  return ;
 }
 
 // Convert a Celsius temperature to a Fahrenheit temperature.
 double celsiusToFahrenheit(double celsiusTemp) {
 
+return (celsiusTemp * 9 / 5) + 32;
+
 }
 
 // Convert a Fahrenheit temperature to a Celsius temperature.
 double fahrenheitToCelsius(double fahrenheitTemp) {
+
+return (fahrenheitTemp - 32) * 5 / 9;
 
 }
 
